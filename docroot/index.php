e2af52279030b0f19084251e8f4e41849c0e2da3
Jason Slavin <jslavin@marketo.com>
2018-02-03 09:45:59 -0800

    Trying

--- docroot/index.php
+++ docroot/index.php
@@ -1,1 +1,14 @@
 
+<h1>Hello</h1>
+
+<?php
+
+try {
+    $albumShareDB = "mysql:dbname=albumShare;host=localhost";
+    $dbUser = "root";
+    $dbPassword = "developer";
+
+    $connection = new PDO($albumShareDB, $dbUser, $dbPassword);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
