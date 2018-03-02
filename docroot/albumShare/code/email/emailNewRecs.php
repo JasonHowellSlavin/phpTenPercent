52742228feefd8819f51855a418c3f82e9f88861
Jason Slavin <jslavin@marketo.com>
2018-03-02 15:36:08 -0800

    Stuff

--- docroot/albumShare/code/email/emailNewRecs.php
+++ docroot/albumShare/code/email/emailNewRecs.php
@@ -24,12 +24,17 @@ $mgClient = new Mailgun('78901d2ba85f304101bd27611542885a');
 $domain = "absh-www.jhslavin.com";
 
 # Make the call to the client.
-$result = $mgClient->sendMessage($domain, array(
-    'from'    => 'Excited User <mailgun@absh-www.jhslavin.com>',
-    'to'      => 'Baz <slavin.jhs@gmail.com>',
-    'subject' => 'Hello',
-    'text'    => 'Testing some Mailgun awesomness!'
-));
+$try {
+    $result = $mgClient->sendMessage($domain, array(
+        'from'    => 'Excited User <mailgun@absh-www.jhslavin.com>',
+        'to'      => 'Baz <slavin.jhs@gmail.com>',
+        'subject' => 'Hello',
+        'text'    => 'Testing some Mailgun awesomness!'
+    ));
+} catch ($e) {
+  echo "Humphrey";
+};
+
 
 ?>
 
