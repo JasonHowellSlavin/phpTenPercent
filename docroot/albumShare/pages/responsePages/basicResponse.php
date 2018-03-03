39e27e57b56b8b67c2ee78e08d8368b31eec1045
Jason Slavin <jslavin@marketo.com>
2018-03-02 16:40:09 -0800

    Another one

--- docroot/albumShare/pages/responsePages/basicResponse.php
+++ docroot/albumShare/pages/responsePages/basicResponse.php
@@ -7,7 +7,7 @@ $loot = $_SERVER['HTTP_HOST'];
 
 echo "<h1>Thank You! Your submission is processed!</h1>";
 
-header("refresh:3;url=phpTenPercent/docroot/albumShare/pages/home.php");
+header("refresh:3;url=http://ec2-52-203-38-207.compute-1.amazonaws.com/phpTenPercent/docroot/albumShare/pages/home.php");
 
 
 
