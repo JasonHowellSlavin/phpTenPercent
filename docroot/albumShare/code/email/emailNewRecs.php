0dce4709b1167735a50bfa50b9459621efae3742
Jason Slavin <jslavin@marketo.com>
2018-03-02 16:39:13 -0800

    Mailgun works now

--- docroot/albumShare/code/email/emailNewRecs.php
+++ docroot/albumShare/code/email/emailNewRecs.php
@@ -19,10 +19,47 @@ $email = (!empty($_SESSION["userEmail"])) ? $_SESSION["userEmail"] : "";
 
 
 # Instantiate the client.
-$mgClient = new Mailgun('78901d2ba85f304101bd27611542885a');
+$mgClient = new Mailgun('key-78901d2ba85f304101bd27611542885a');
 $domain = "absh-www.jhslavin.com";
 
 echo gettype($mgClient);
+
+
+$message = "
+    <html>
+    <head>
+        <title>Your Reccomendations from </title>
+    </head>
+    <body>
+        <table style='border-spacing:20px;'>
+            <tbody>
+                <tr>
+                   <td> Hey dude
+                   </td>
+                </tr>
+                <tr>
+                    <td>
+                    You just got some new recs!
+                    </td>
+                </tr>
+                <tr>
+                     <td>
+                     Here they are:
+                        <table>
+                            <tr>
+                               <td>
+                               Artist:
+                               </td>
+                               <td>
+                               Album:
+                               </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+    </body>";
+
 
 # Make the call to the client.
 try {
@@ -30,63 +67,8 @@ try {
         'from'    => 'Excited User <mailgun@absh-www.jhslavin.com>',
         'to'      => 'Baz <slavin.jhs@gmail.com>',
         'subject' => 'Hello',
-        'text'    => 'Testing some Mailgun awesomness!'
+        'html'    => $message,
     ));
 } catch (Exception $e){
   echo "Humphrey, $e";
 };
-
-?>
-
-<h1>Yo</h1>
-<?php
-//public function ($postUserEmail, $postRecArtist, $postRecAlbum, $numRecs) {
-//
-//    get
-//
-//    $mesage = "
-//    <html>
-//    <head>
-//        <title>Your Reccomendations from $friendId</title>
-//    </head>
-//    <body>
-//        <table style='border-spacing:20px;'>
-//            <tbody>
-//                <tr>
-//                   <td> Hey $name
-//                   </td>
-//                </tr>
-//                <tr>
-//                    <td>
-//                    You just got some new recs!
-//                    </td>
-//                </tr>
-//                <tr>
-//                     <td>
-//                     Here they are:
-//                        <table>
-//                            <tr>
-//                               <td>
-//                               Artist:
-//                               </td>
-//                               <td>
-//                               Album:
-//                               </td>
-//                            </tr>
-//
-//
-//
-//
-//
-//
-//
-//                        </table>
-//                    </td>
-//                </tr>
-//            </tbody>
-//    </body>";
-//}
-
-
-
-
--- docroot/albumShare/pages/responsePages/shareAlbumResponse.php
+++ docroot/albumShare/pages/responsePages/shareAlbumResponse.php
@@ -1,6 +1,12 @@
 <?php
 require '../../code/pdo.php';
+require '../../../vendor/autoload.php';
 session_start();
+
+use Mailgun\Mailgun;
+
+$mgClient = new Mailgun('key-78901d2ba85f304101bd27611542885a');
+$domain = "absh-www.jhslavin.com";
 
 $number =  $_POST["number-of-recs"];
 $friendIDNum = $_POST["friendID"];
@@ -11,9 +17,56 @@ $email = (!empty($_SESSION["userEmail"])) ? $_SESSION["userEmail"] : "";
 
 $connect = pdoConnect::connectToDB();
 
-if (!$connect) {
-    echo "Could not connect";
+$getFriendID = $connect->prepare("SELECT * FROM users WHERE userId = ?;");
+$getFriendID->bindParam(1,$friend);
+$friend = $friendIDNum;
+$getFriendIDStatement = $getFriendID->execute();
+$friendData = $getFriendID->fetchAll(PDO::FETCH_ASSOC);
+
+$friendName = $friendData[0]['userName'];
+$friendEmail = $friendData[0]['userEmail'];
+
+print_r($friendData);
+
+echo '<h1> Friend name ' . $friendName . $friendEmail . '</h1>';
+
+
+$message = "
+   <html>
+    <head>
+        <title>You have new recommendations from " . $name . "</title>
+    </head>
+    <body>
+        <table style='border-spacing:20px;'>
+            <tbody>
+                <tr>
+                   <td> Hey " . $friendName . "
+                   </td>
+                </tr>
+                <tr>
+                    <td style='margin-top: 20px;'>
+                    You just got some new recs! Go check em out at: http://ec2-52-203-38-207.compute-1.amazonaws.com/phpTenPercent/docroot/albumShare/pages/loginPages/initialLogin.php
+                    </td>
+                </tr>
+            </tbody>
+    </body>
+";
+
+try {
+    $result = $mgClient->sendMessage($domain, array(
+        'from'    => 'albumShare <rockNRolla@absh-www.jhslavin.com>',
+        'to'      => $friendEmail,
+        'subject' => 'You Have Some New Album Recommendations from ' . $name .  '!',
+        'html'    => $message,
+    ));
+} catch (Exception $e){
+    echo "Humphrey, $e";
 };
+
+
+
+
+
 
 $addRecToDb = $connect->prepare("INSERT INTO userRecommendations VALUES (null, ?, ?, ? ,?, ?, ?)");
 
